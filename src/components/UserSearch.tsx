709610cddbeec51d9804bb56698588c8db082348
Mian Jazib Ali <m.mianjazibali@gmail.com>
2022-04-27 15:39:28 +0200

    Setup basic redux for user search

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,16 +1,27 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, FormControl } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+
+import { RootState } from '../store';
+import { setSearch } from './../features/search/searchSlice';
 
 const UserSearch = () => {
+    const search = useSelector((state : RootState) => state.search.value);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     return (
         <div className='d-flex justify-content-center position-fixed top-50 left-50 start-50 translate-middle'>
             <FormControl
                 className='shadow-none d-inline w-auto float-start me-3'
                 placeholder='Find a user...'
+                value={search}
+                onChange={evt => dispatch(setSearch(evt.target.value))}
             />
 
-            <Button variant='success'>
+            <Button variant='success' onClick={() => navigate(search)}>
                 <FontAwesomeIcon className='me-2' icon={faSearch} />
                 Search
             </Button>
---
+++ src/features/search/searchSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface SearchState {
+  value: string
+};
+
+const initialState: SearchState = {
+  value: ''
+};
+
+export const counterSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setSearch: (state, action : PayloadAction<string>) => {
+      state.value = action.payload;
+    }
+  },
+});
+
+export const { setSearch } = counterSlice.actions;
+
+export default counterSlice.reducer;
